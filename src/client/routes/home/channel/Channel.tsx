a8c6fb06edbabe075ed77e8985b1593a1b82df62
Zachary C <not.an.email@example.com>
2022-11-23 19:17:05 +0900

    Show chat list on channel route

--- src/client/routes/home/channel/Channel.tsx
+++ src/client/routes/home/channel/Channel.tsx
@@ -1,3 +1,9 @@
+import { useParams } from "react-router-dom";
+import Chat from "../../../components/chat/Chat";
+
 export default function Channel() {
-  return null;
+  const { channelId: channelIdString } = useParams();
+  const channelId = parseInt(channelIdString || "", 10);
+  if (Number.isNaN(channelId)) throw new Error("Invalid channel id.");
+  return <Chat channelId={channelId} />;
 }
