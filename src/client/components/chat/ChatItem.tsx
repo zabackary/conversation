9e89a8535bc01e2167fe987f0e2382a11fbcfb6c
Zachary C <not.an.email@example.com>
2022-12-01 13:26:40 +0900

    Add time ago to messages

--- src/client/components/chat/ChatItem.tsx
+++ src/client/components/chat/ChatItem.tsx
@@ -1,10 +1,17 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import {
+  Avatar,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { Fragment } from "react";
 import ReactMarkdown from "react-markdown";
 import {
   PluggableList,
   ReactMarkdownOptions,
 } from "react-markdown/lib/react-markdown";
+import TimeAgo from "react-timeago";
 import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
 import Message from "../../../data/message";
@@ -33,7 +40,14 @@ export default function ChatItem({ message, showAvatar }: Props) {
         <Avatar src={message.user.profilePicture} alt={message.user.name} />
       </ListItemAvatar>
       <ListItemText
-        primary={message.user.nickname}
+        primary={
+          <>
+            {message.user.nickname}{" "}
+            <Typography variant="body2" component="span" sx={{ opacity: 0.5 }}>
+              <TimeAgo date={message.sent} />
+            </Typography>
+          </>
+        }
         secondary={
           <NoPaddingReactMarkdown>{message.markdown}</NoPaddingReactMarkdown>
         }
