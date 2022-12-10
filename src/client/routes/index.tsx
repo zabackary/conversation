8250b2d33bf76e59041638283ea9334dbd885843
Zachary C <not.an.email@example.com>
2022-12-10 16:34:32 +0900

    Add dummy login page

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,13 +1,15 @@
+import { RouteObject } from "react-router-dom";
 import ErrorRoute from "./error";
 import HomeRoute from "./home";
 import ChannelRoute from "./home/channel";
 import DashboardRoute from "./home/DashboardRoute";
 import DmRoute from "./home/dm";
 import SettingsRoute from "./home/settings";
+import LoginRoute from "./login";
 
-const routes = [
+const routes: RouteObject[] = [
   {
-    path: "/",
+    path: "",
     element: <HomeRoute />,
     errorElement: <ErrorRoute />,
     children: [
@@ -30,6 +32,12 @@ const routes = [
         element: <SettingsRoute />,
       },
     ],
+  },
+  {
+    path: "login",
+    element: <LoginRoute />,
+    errorElement: <ErrorRoute />,
+    children: [{ path: "create", element: null }],
   },
 ];
 export default routes;
---
+++ src/client/routes/login/index.tsx
@@ -0,0 +1,4 @@
+export default function LoginRoute() {
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <></>;
+}
