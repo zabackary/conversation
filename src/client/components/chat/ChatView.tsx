6d1269687307476a144fcdda2fe77bbd07afc08d
Zachary C <not.an.email@example.com>
2022-12-10 14:33:03 +0900

    Elevate input on scroll and show content after msg

--- src/client/components/chat/ChatView.tsx
+++ src/client/components/chat/ChatView.tsx
@@ -1,4 +1,5 @@
 import { Box, SxProps } from "@mui/material";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Message from "../../../data/message";
 import ChatInput, { MessageEvent } from "./ChatInput";
 import ChatList from "./ChatList";
@@ -9,6 +10,7 @@ export interface ChatViewProps {
   onSend: (event: MessageEvent) => void;
   username?: string;
   channelName?: string;
+  afterInput?: ReactNode;
   sx?: SxProps;
 }
 
@@ -18,9 +20,33 @@ export default function ChatView({
   username,
   channelName,
   sx,
+  afterInput,
 }: ChatViewProps) {
+  const [isSticky, setIsSticky] = useState(false);
+  const inputRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!inputRef.current || !containerRef.current) return undefined;
+    const cachedRef = inputRef.current;
+    const observer = new IntersectionObserver(
+      ([e]) => setIsSticky(e.intersectionRatio < 1),
+      {
+        rootMargin: "0px 0px -1px 0px",
+        threshold: 1,
+        root: containerRef.current,
+      }
+    );
+
+    observer.observe(cachedRef);
+
+    return () => {
+      observer.unobserve(cachedRef);
+    };
+  }, [inputRef, containerRef]);
+
   return (
-    <Box sx={sx}>
+    <Box sx={sx} ref={containerRef}>
       {messages ? <ChatList messages={messages} /> : <ChatListSkeleton />}
       <ChatInput
         onMessageSend={(event) => {
@@ -29,13 +55,17 @@ export default function ChatView({
         sx={{
           position: "sticky",
           bottom: "24px",
+          marginBottom: afterInput ? "12px" : "0px",
         }}
         placeholder={
           channelName && username
             ? `Message ${channelName} as ${username}...`
             : ""
         }
+        elevate={!isSticky}
+        ref={inputRef}
       />
+      {afterInput}
     </Box>
   );
 }
