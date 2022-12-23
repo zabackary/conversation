a17fe5bf9fc5ffabc792f25a82110c06e1e5a987
Zachary C <not.an.email@example.com>
2022-12-23 17:08:10 +0900

    Set language

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "./components/error";
 
+document.documentElement.lang = "en-US";
 document.body.style.margin = "0";
 document.body.style.setProperty("-webkit-tap-highlight-color", "transparent");
 const container = document.createElement("div");
