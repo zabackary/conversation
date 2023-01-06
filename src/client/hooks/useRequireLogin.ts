096943f303ab94a9f75bcd815ffcaa4893065b4d
Zachary C <not.an.email@example.com>
2023-01-06 18:20:38 +0900

    Simplify useRequireLogin

--- src/client/hooks/useRequireLogin.ts
+++ src/client/hooks/useRequireLogin.ts
@@ -1,19 +1,5 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import useUser from "./useUser";
 
 export default function useRequireLogin() {
-  const user = useUser(true);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) {
-      navigate(
-        `/login/?next=${encodeURIComponent(
-          window.location.pathname +
-            window.location.hash +
-            window.location.search
-        )}`
-      );
-    }
-  }, [user, navigate]);
+  useUser(true);
 }
