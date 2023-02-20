71c89dbc246ef6da61e28e4c5af09ca06f07c186
Zachary C <not.an.email@example.com>
2023-02-20 09:54:29 +0900

    Add hash function & documentation to server utils

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 /**
  * https://stackoverflow.com/a/64123628
  *
@@ -16,5 +15,41 @@ export function keepIfInEnum<T>(
   return undefined;
 }
 
+/**
+ * https://stackoverflow.com/a/51399781
+ *
+ * Convert an `Array` to its element type.
+ */
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+/**
+ * `cyrb53` (c) 2018 [bryc](github.com/bryc)
+ *
+ * A fast and simple hash function with decent collision resistance.
+ * Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
+ * Public domain. Attribution appreciated.
+ */
+export function cyrb53(str: string, seed = 0) {
+  /* eslint-disable no-bitwise */
+  let h1 = 0xdeadbeef ^ seed;
+  let h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i += 1) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+    Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+    Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return (
+    (h2 >>> 0).toString(16).padStart(8, "0") +
+    (h1 >>> 0).toString(16).padStart(8, "0")
+  );
+  /* eslint-enable no-bitwise */
+}
