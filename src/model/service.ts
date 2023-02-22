8daf0c82da4d7eae96a7426e9159455ee8c85da6
Zachary C <not.an.email@example.com>
2023-02-22 17:28:23 +0900

    Update Service types to be more consistant

--- src/model/service.ts
+++ src/model/service.ts
@@ -9,15 +9,17 @@ export default interface Service {
   /**
    * Icon for service (shown on messages, etc.)
    */
-  icon: string;
+  icon: string | null;
 
   /**
    * Banner for service (shown on info screens)
    */
-  banner: string;
+  banner: string | null;
 
   /**
    * The user id of the author, or `null` if built-in.
    */
   author: User | null;
+
+  id: number;
 }
