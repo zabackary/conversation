b169275a2850ffe0462c390d89ffa59d80afe3f1
Zachary C <not.an.email@example.com>
2023-02-26 15:07:47 +0900

    Change test for server

--- src/server/test.ts
+++ src/server/test.ts
@@ -1,5 +1,21 @@
-import migrateDatabase from "./database/migrateDatabase";
+import getDatabaseHandle from "./database";
+import schema from "./database/schema";
 
 export default function test() {
-  migrateDatabase();
+  let last = new Date();
+  const logTime = (tag = "default") => {
+    console.log(`+${new Date().getTime() - last.getTime()}ms - ${tag}`);
+    last = new Date();
+  };
+
+  logTime("start");
+
+  const database = getDatabaseHandle();
+  logTime("opened");
+
+  database.migrate(schema);
+  logTime("migrated");
+
+  database.flush();
+  logTime("flushed");
 }
