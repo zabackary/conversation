583ef7bc4f9472edaf206e6f442bbf1764b71a80
Zachary C <not.an.email@example.com>
2023-03-02 08:27:55 +0900

    Add i18next scaffold

---
+++ src/client/i18n.ts
@@ -0,0 +1,21 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import resources from "../translations.json";
+
+i18next
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+  .then((_t) => {
+    console.log("Initialized i18next");
+  })
+  .catch(() => {
+    console.error("! Failed to initialize i18next");
+  });
+
+export default i18next;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "./components/error";
+import "./i18n";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Cannot find root");
