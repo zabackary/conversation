026fb64925c0cc2616eb7085283f947cd18b792d
Zachary C <not.an.email@example.com>
2023-03-17 18:32:35 +0900

    Delete all server-side files, except web app

--- src/server/libDatabase/Schema.ts
+++
@@ -1,9 +0,0 @@
-import Entity from "./Entity";
-
-export default interface Schema {
-  version: number;
-  entities: Record<
-    string,
-    { new (sheet: GoogleAppsScript.Spreadsheet.Spreadsheet): Entity }
-  >;
-}
--- src/server/libDatabase/index.ts
+++
@@ -1,5 +0,0 @@
-import loadDatabase from "./loadDatabase";
-
-export default loadDatabase;
-export { default as makeSchema } from "./makeSchema";
-export type { default as Schema } from "./Schema";
