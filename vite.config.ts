9bec65e99fa3c3894efd7707feba7c02547e9d9c
Zachary C <not.an.email@example.com>
2023-03-18 12:55:53 +0900

    Use Supabase in dev

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig(({ command, mode, ssrBuild: _ssrBuild }) => {
           replacement: `network/${
             env.NODE_ENV === "production"
               ? "supabase/index.ts"
-              : "mock/index.ts"
+              : "supabase/index.ts"
           }`,
         },
       ],
