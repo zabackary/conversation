78c64c5a7f4c5d42ac6e67ed33ef57f3abe58652
Zachary C <not.an.email@example.com>
2023-03-21 20:01:59 +0900

    Update convertUser

--- src/client/network/supabase/converters/convertUser.ts
+++ src/client/network/supabase/converters/convertUser.ts
@@ -1,15 +1,36 @@
-import User, { UserState, UserStatus } from "../../../../model/user";
+import User, { PrivilegeLevel } from "../../../../model/user";
 import { SupabaseUser } from "../cache";
 
 export default function convertUser(dbUser: SupabaseUser): User {
+  // TODO: Add status, banner, etc. to Supabase
+  if (dbUser.is_bot) {
+    return {
+      id: dbUser.id,
+      isBot: true,
+      name: dbUser.name,
+      privilegeLevel: dbUser.trusted
+        ? PrivilegeLevel.Normal
+        : PrivilegeLevel.Unverified,
+      active: false,
+      author: undefined,
+      banner: undefined,
+      email: undefined,
+      profilePicture: dbUser.profile_picture_url ?? undefined,
+    };
+  }
   return {
+    id: dbUser.id,
+    isBot: false,
     name: dbUser.name,
     nickname: dbUser.nickname,
-    email: "unknown", // TODO: Add email to Supabase
-    profilePicture: dbUser.profile_picture_url,
-    id: dbUser.id,
-    status: UserStatus.Inactive,
-    state: dbUser.trusted ? UserState.Normal : UserState.Unverified,
-    banner: null,
+    privilegeLevel: dbUser.trusted
+      ? PrivilegeLevel.Normal
+      : PrivilegeLevel.Unverified,
+    active: false,
+    banner: undefined,
+    email: undefined,
+    profilePicture: dbUser.profile_picture_url ?? undefined,
+    status: undefined,
+    tag: undefined,
   };
 }
