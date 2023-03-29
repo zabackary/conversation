5ecbc438ea19b6601b29911f9454bde107a74c03
Zachary C <not.an.email@example.com>
2023-03-29 12:36:49 +0900

    Add M3Theme for MuiToggleButton

--- src/client/theme/m3/M3Theme.ts
+++ src/client/theme/m3/M3Theme.ts
@@ -488,6 +488,38 @@ export const getThemedComponents = (
           },
         },
       },
+      MuiToggleButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: "40px",
+            textTransform: "none",
+            borderColor: theme.palette.outline,
+            paddingX: "12px",
+            "&::before": {
+              content: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+                theme.palette.onSecondaryContainer.main
+              )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+              width: "0px",
+              marginRight: "0px",
+              transform: "scale(0) translate(-12px, 3px)",
+              transition: theme.transitions.create([
+                "width",
+                "transform",
+                "marginRight",
+              ]),
+            },
+            "&.Mui-selected": {
+              backgroundColor: theme.palette.secondaryContainer.main,
+              color: theme.palette.secondaryContainer.contrastText,
+              "&::before": {
+                width: "24px",
+                marginRight: "12px",
+                transform: "scale(1) translate(0px, 3px)",
+              },
+            },
+          },
+        },
+      },
       MuiButton: {
         styleOverrides: {
           root: {
