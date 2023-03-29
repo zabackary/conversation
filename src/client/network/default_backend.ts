30dd8e9853cb77f48deec1f77db4f3075fbcf190
Zachary C <not.an.email@example.com>
2023-03-29 12:32:59 +0900

    Add easier toggle switch thingy

--- src/client/network/default_backend.ts
+++ src/client/network/default_backend.ts
@@ -3,7 +3,11 @@
 import MockBackend from "./mock";
 import SupabaseBackend from "./supabase";
 
+const USE_SUPABASE_IN_DEVELOPMENT = true as boolean;
+
 const DefaultBackend =
-  process.env.NODE_ENV === "production" ? SupabaseBackend : MockBackend;
+  process.env.NODE_ENV === "production" || USE_SUPABASE_IN_DEVELOPMENT
+    ? SupabaseBackend
+    : MockBackend;
 
 export default DefaultBackend;
