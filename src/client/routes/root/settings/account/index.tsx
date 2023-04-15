2f94e08edce55012b4b46ca5da8faba4b8aad15c
Zachary C <not.an.email@example.com>
2023-04-15 16:00:19 +0900

    Navigate to the landing on log out

--- src/client/routes/root/settings/account/index.tsx
+++ src/client/routes/root/settings/account/index.tsx
@@ -1,15 +1,29 @@
 import LogoutIcon from "@mui/icons-material/Logout";
 import { Button, Grid } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import { ConversationAppBar } from "../../../../components/layout";
 import { BaseItem } from "../../../../components/settings";
+import useSnackbar from "../../../../components/useSnackbar";
 import useBackend from "../../../../hooks/useBackend";
 import useUser from "../../../../hooks/useUser";
 
 export default function AccountSettingsRoute() {
   const { t } = useTranslation("settings");
+  const { showSnackbar } = useSnackbar();
   const user = useUser();
   const backend = useBackend();
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    backend
+      .authLogOut()
+      .then(() => {
+        navigate("/");
+      })
+      .catch(() => {
+        showSnackbar(t("error", { ns: "general" }));
+      });
+  };
   if (!user) return <>Logged out</>;
   return (
     <>
@@ -22,7 +36,7 @@ export default function AccountSettingsRoute() {
             <Button
               variant="tonal"
               startIcon={<LogoutIcon />}
-              onClick={() => void backend.authLogOut()}
+              onClick={handleLogOut}
             >
               Log out
             </Button>
