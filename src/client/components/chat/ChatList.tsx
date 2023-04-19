362643688e3140fda9f037c6151a727c5c5411b6
Zachary C <not.an.email@example.com>
2023-04-19 21:37:19 +0900

    Remove broken msg list stuff

--- src/client/components/chat/ChatList.tsx
+++ src/client/components/chat/ChatList.tsx
@@ -1,5 +1,4 @@
-import { Collapse, List, SxProps } from "@mui/material";
-import { TransitionGroup } from "react-transition-group";
+import { List, SxProps } from "@mui/material";
 import Message from "../../../model/message";
 import ChatItem from "./ChatItem";
 
@@ -13,25 +12,21 @@ interface Props {
 export default function ChatList({ messages, sx }: Props) {
   return (
     <List sx={sx}>
-      <TransitionGroup>
-        {messages.map((message, index) => {
-          const lastMessage = messages[index - 1];
-          return (
-            <Collapse key={message.id}>
-              <ChatItem
-                message={message}
-                showAvatar={
-                  index === 0 || lastMessage.isService || message.isService
-                    ? true
-                    : lastMessage.user.id !== message.user.id ||
-                      message.sent.getTime() - lastMessage.sent.getTime() >
-                        COMBINE_MESSAGES_THRESHOLD
-                }
-              />
-            </Collapse>
-          );
-        })}
-      </TransitionGroup>
+      {messages.map((message, index) => {
+        const lastMessage = messages[index - 1];
+        return (
+          <ChatItem
+            message={message}
+            showAvatar={
+              index === 0 || lastMessage.isService || message.isService
+                ? true
+                : lastMessage.user.id !== message.user.id ||
+                  message.sent.getTime() - lastMessage.sent.getTime() >
+                    COMBINE_MESSAGES_THRESHOLD
+            }
+          />
+        );
+      })}
     </List>
   );
 }
