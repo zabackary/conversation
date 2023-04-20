91bc54e4b613eb4aa05f784c5a85cbf905291c4c
Zachary C <not.an.email@example.com>
2023-04-20 20:40:26 +0900

    Add channel edit screen

--- src/client/components/chat/InfoMenu.tsx
+++ src/client/components/chat/InfoMenu.tsx
@@ -1,6 +1,12 @@
 import {
   Box,
+  Button,
   Collapse,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Stack,
   SxProps,
   TextField,
@@ -8,10 +14,13 @@ import {
   ToggleButtonGroup,
   Typography,
 } from "@mui/material";
-import { useMemo, useState } from "react";
+import { FormEvent, useMemo, useState } from "react";
 import { shallowEqualObjects } from "shallow-equal";
 import Channel, { PrivacyLevel } from "../../../model/channel";
 import LoadingButton from "../LoadingButton";
+import useBackend from "../../hooks/useBackend";
+import useSnackbar from "../useSnackbar";
+import useUser from "../../hooks/useUser";
 
 export interface InfoMenuProps {
   channel: Channel;
@@ -19,6 +28,9 @@ export interface InfoMenuProps {
 }
 
 export default function InfoMenu({ channel, sx }: InfoMenuProps) {
+  const user = useUser();
+  const canEdit =
+    channel.dm || channel.membersCanEdit || channel.owner === user?.id;
   const [pendingChange, setPendingChange] = useState<Channel>(() => channel);
   const handleChange =
     <
@@ -49,24 +61,74 @@ export default function InfoMenu({ channel, sx }: InfoMenuProps) {
     () => !shallowEqualObjects(pendingChange, channel),
     [pendingChange, channel]
   );
+  const backend = useBackend();
+  const { showSnackbar } = useSnackbar();
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    backend
+      .updateChannel(channel.id, {
+        description: pendingChange.description,
+        privacyLevel: pendingChange.privacyLevel,
+        name: pendingChange.name,
+      })
+      .then(() => {
+        showSnackbar("Changes saved.");
+      })
+      .catch(() => {
+        showSnackbar("Failed to save changes.");
+      });
+  };
+  const [deletionOpen, setDeletionOpen] = useState(false);
+  const handleDeleteChannel = () => {
+    setDeletionOpen(false);
+    backend
+      .deleteChannel(channel.id)
+      .then(() => {
+        showSnackbar("Channel deleted.");
+      })
+      .catch(() => {
+        showSnackbar("Failed to delete channel.");
+      });
+  };
   return (
-    <Stack sx={sx} spacing={2} component="form">
+    <Stack sx={sx} spacing={2} component="form" onSubmit={handleSubmit}>
+      <Dialog open={deletionOpen} onClose={() => setDeletionOpen(false)}>
+        <DialogTitle>Delete channel?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this channel? This action{" "}
+            <em>cannot be undone</em>. All associated messages and their
+            attachments, images, and more will be <em>deleted forever</em>.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDeletionOpen(false)}>Cancel</Button>
+          <Button onClick={handleDeleteChannel}>Confirm</Button>
+        </DialogActions>
+      </Dialog>
       <TextField
         label="Name"
         value={pendingChange.name}
         onChange={handleChange("name")}
+        InputProps={{
+          readOnly: !canEdit,
+        }}
       />
       <TextField
         label="Description"
         value={pendingChange.description}
         onChange={handleChange("description")}
         multiline
+        InputProps={{
+          readOnly: !canEdit,
+        }}
       />
       <ToggleButtonGroup
         value={pendingChange.privacyLevel}
         exclusive
         onChange={handlePrivacyLevelChange}
         fullWidth
+        disabled={!canEdit}
       >
         <ToggleButton value={PrivacyLevel.Private}>Private</ToggleButton>
         <ToggleButton value={PrivacyLevel.Unlisted}>Unlisted</ToggleButton>
@@ -81,22 +143,29 @@ export default function InfoMenu({ channel, sx }: InfoMenuProps) {
           value={pendingChange.passphrase ?? ""}
           onChange={handleChange("passphrase")}
           fullWidth
+          disabled={!channel.dm && !channel.membersCanEdit}
         />
       </Collapse>
-      <Box>
-        <LoadingButton
-          disabled={!isPendingChanges}
-          variant="filled"
-          type="submit"
-        >
-          Save
-        </LoadingButton>{" "}
-        {isPendingChanges ? (
-          <Typography variant="caption" fontStyle="italic">
-            Pending changes
-          </Typography>
-        ) : null}
-      </Box>
+      {canEdit ? (
+        <Box>
+          <LoadingButton
+            disabled={!isPendingChanges}
+            variant="filled"
+            type="submit"
+          >
+            Save
+          </LoadingButton>{" "}
+          {isPendingChanges ? (
+            <Typography variant="caption" fontStyle="italic">
+              Pending changes
+            </Typography>
+          ) : (
+            <Button variant="tonal" onClick={() => setDeletionOpen(true)}>
+              Delete channel
+            </Button>
+          )}
+        </Box>
+      ) : null}
     </Stack>
   );
 }
