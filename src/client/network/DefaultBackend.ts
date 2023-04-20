359d590ca50a30c02573247f99be30919c031c41
Zachary C <not.an.email@example.com>
2023-04-20 17:59:28 +0900

    Abstract DefaultBackend more

--- src/client/network/DefaultBackend.ts
+++ src/client/network/DefaultBackend.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable class-methods-use-this */
+import NetworkBackend from "./NetworkBackend";
 import MockBackend from "./mock";
 import SupabaseBackend from "./supabase";
 
 const USE_SUPABASE_IN_DEVELOPMENT = true as boolean;
 
-const DefaultBackend =
+const DefaultBackend: new () => NetworkBackend =
   process.env.NODE_ENV === "production" || USE_SUPABASE_IN_DEVELOPMENT
     ? SupabaseBackend
     : MockBackend;
