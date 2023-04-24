ad11c88d50de5ccf77cb82f72af54d8c3deea1c5
Zachary C <not.an.email@example.com>
2023-04-24 19:36:47 +0900

    Fix spacing of large `NavigationRailAction`s

--- src/client/components/NavigationRail/NavigationRailAction.tsx
+++ src/client/components/NavigationRail/NavigationRailAction.tsx
@@ -4,6 +4,8 @@ const NavigationRailAction = styled(BottomNavigationAction)(() => ({
   height: "56px",
   margin: "12px 0 12px 0",
   flexGrow: "0",
+  width: "88px",
+  textAlign: "center",
   "& .MuiSvgIcon-root": {
     // There's custom styling oin the m3 theme to hide the ripple, but it
     // doesn't play well with the alpha since the navigation rail needs to be
