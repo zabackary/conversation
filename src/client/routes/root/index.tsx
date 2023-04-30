92773d8971644cb9b1a20190c3422f34eb13364d
Zachary C <not.an.email@example.com>
2023-04-30 16:16:53 +0900

    Add UI for disabled accounts

--- src/client/routes/root/index.tsx
+++ src/client/routes/root/index.tsx
@@ -1,16 +1,23 @@
 import {
+  Avatar,
   Box,
+  Button,
+  Container,
   Fade,
+  Grid,
+  Link,
   List,
   Paper,
   Stack,
+  Typography,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useMatches, useOutlet } from "react-router-dom";
+import { useMatches, useOutlet, Link as RouterLink } from "react-router-dom";
 import { SwitchTransition } from "react-transition-group";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import {
   ConversationNavigationRail,
   navigationRailWidth,
@@ -21,7 +28,7 @@ import useSnackbar from "../../components/useSnackbar";
 import useBackend from "../../hooks/useBackend";
 import useRouteForward from "../../hooks/useRouteForward";
 import { useSubscribable } from "../../hooks/useBackendFunction";
-import useRequireLogin from "../../hooks/useRequireLogin";
+import useUser from "../../hooks/useUser";
 
 function LoadingGlimmer() {
   return (
@@ -43,7 +50,7 @@ function LoadingGlimmer() {
 
 export default function RootRoute() {
   useRouteForward();
-  useRequireLogin();
+  const user = useUser(true);
   const match = useMatches()[1] as ReturnType<typeof useMatches>[2] | undefined;
   const currentOutlet = useOutlet();
   const theme = useTheme();
@@ -99,6 +106,75 @@ export default function RootRoute() {
       });
     }
   }, [connectionState, showSnackbar]);
+  if (user?.disabled)
+    return (
+      <Container component="main" maxWidth="xs" sx={{ pt: 6, pb: 8 }}>
+        <Paper
+          variant="outlined"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            p: { xs: 2, md: 3 },
+            borderRadius: 3,
+            textAlign: "center",
+          }}
+        >
+          <Avatar
+            sx={{
+              m: 1,
+              bgcolor: "error.main",
+              height: 76,
+              width: 76,
+              fontSize: 48,
+            }}
+          >
+            &#128557;
+          </Avatar>
+          <Typography component="h1" variant="h5" mb={1}>
+            Your account has been disabled
+          </Typography>
+          <Typography variant="body1" mb={1}>
+            Something went awry. It seems your account has been disabled. If you
+            believe this to be in error, please{" "}
+            <Link href="mailto:TODO@example.com" target="_blank">
+              contact us
+            </Link>{" "}
+            to get it fixed as soon as possible.
+          </Typography>
+          <Typography variant="body2" mb={1}>
+            Repeat offenders&apos; accounts may be deleted permanently from our
+            databases without prior notice. Remember to abide by our Terms of
+            Service at all times while using Conversation.
+          </Typography>
+          <Typography variant="body2">
+            Conversation utilizes automatic spam-prevention technologies to
+            avoid spam. Your account may have been disabled in error by
+            automated proccesses to avoid{" "}
+            <Link
+              href="https://www.cloudflare.com/learning/ddos/what-is-a-ddos-attack/"
+              target="_blank"
+            >
+              DDoS
+            </Link>
+            . It can be re-enabled by administrators.
+          </Typography>
+          <Grid container width="100%" textAlign="left">
+            <Grid item xs>
+              <Button
+                component={RouterLink}
+                to="/"
+                variant="tonal"
+                sx={{ mt: 3, mb: 2 }}
+                startIcon={<ArrowBackIcon />}
+              >
+                Back
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    );
   return isReady ? (
     <Stack direction={isMobile ? "column" : "row"} height="100%">
       <Box width={navigationRailWidth}>
