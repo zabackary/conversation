375061e3c70f325a11a9e4a2a7b0d993d255bb15
Zachary C <not.an.email@example.com>
2023-05-14 18:04:47 +0900

    Move log out button

--- src/client/routes/root/settings/account/index.tsx
+++ src/client/routes/root/settings/account/index.tsx
@@ -1,12 +1,13 @@
-import { Button, List } from "@mui/material";
+import { Button, List, ListItem } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { PrivilegeLevel } from "../../../../../model/user";
+import MaterialSymbolIcon from "../../../../components/MaterialSymbolIcon";
 import { ConversationAppBar } from "../../../../components/layout";
 import { BaseItem } from "../../../../components/settings";
 import useSnackbar from "../../../../components/useSnackbar";
 import useBackend from "../../../../hooks/useBackend";
 import useUser from "../../../../hooks/useUser";
-import MaterialSymbolIcon from "../../../../components/MaterialSymbolIcon";
 
 export default function AccountSettingsRoute() {
   const { t } = useTranslation("settings");
@@ -24,23 +25,40 @@ export default function AccountSettingsRoute() {
         showSnackbar(t("error", { ns: "general" }));
       });
   };
+  const handleContact = () => {
+    window.open(
+      `mailto:${import.meta.env.CLIENT_FEEDBACK_EMAIL ?? ""}`,
+      "_blank"
+    );
+  };
   if (!user) return <>Logged out</>;
   return (
     <>
       <ConversationAppBar title="Account" />
       <List>
+        <ListItem disableGutters>
+          <Button
+            variant="filled"
+            startIcon={<MaterialSymbolIcon icon="logout" />}
+            onClick={handleLogOut}
+          >
+            Log out
+          </Button>
+          <Button
+            variant="tonal"
+            startIcon={<MaterialSymbolIcon icon="mail" />}
+            onClick={handleContact}
+            sx={{ ml: 1 }}
+          >
+            {user.privilegeLevel === PrivilegeLevel.Unverified
+              ? "Request verification"
+              : "Request name change"}
+          </Button>
+        </ListItem>
         <BaseItem
           label={user.name}
           description={user.email ?? user.nickname ?? String(user.id)}
-          control={
-            <Button
-              variant="tonal"
-              startIcon={<MaterialSymbolIcon icon="logout" />}
-              onClick={handleLogOut}
-            >
-              Log out
-            </Button>
-          }
+          control={null}
         />
       </List>
     </>
