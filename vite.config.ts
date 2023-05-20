dc91aa6d5effce9a0bc4928d68b146866db4413f
Zachary C <not.an.email@example.com>
2023-05-20 21:16:57 +0900

    feat: Add tree mapper

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
 import packageJson from "./package.json";
@@ -61,6 +62,11 @@ export default defineConfig(({ command, mode, ssrBuild: _ssrBuild }) => {
         },
       }),
       reactAxe(),
+      visualizer({
+        template: "treemap",
+        open: true,
+        filename: "treemap.html",
+      }),
       ...(command === "build"
         ? [
             gasTopLevel({
