1127b44fb5995ee451d1fe86a4adb3e623255485
Zachary C <not.an.email@example.com>
2023-05-20 21:47:19 +0900

    fix: Fix inlineScript with dynamic import

--- plugins/inlineScript.ts
+++ plugins/inlineScript.ts
@@ -1,31 +1,45 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { PluginOption } from "vite";
+import { unlink, writeFile } from "node:fs/promises";
+import { resolve } from "node:path";
+import { PluginOption, ResolvedConfig } from "vite";
 
 export default function inlineScript(): PluginOption {
+  let viteConfig: ResolvedConfig | undefined;
   return {
     name: "inline-script",
     enforce: "post",
-    generateBundle(_, bundle) {
+    apply: "build",
+    configResolved(config) {
+      viteConfig = config;
+    },
+    async writeBundle(_, bundle) {
+      if (!viteConfig) throw new Error("Config not resolved!");
+      const { root } = viteConfig;
+      const outDir = viteConfig.build.outDir || "dist";
       const htmlFiles = Object.keys(bundle).filter((i) => i.endsWith(".html"));
       const bundlesToDelete: string[] = [];
       for (const name of htmlFiles) {
         const chunk = bundle[name];
         if (chunk.type === "asset" && typeof chunk.source === "string") {
-          chunk.source = chunk.source.replaceAll(
-            /<script([\s\S]*?) src=["']([\s\S]*?)["']([\s\S]*?)>[\s\S]*?<\/script>/gi,
-            (_, beforeSrc: string, src: string, afterSrc: string) => {
-              const source = bundle[src.slice(1)];
-              if (!source || source.type !== "chunk")
-                throw new Error("Cannot find script chunk");
-              bundlesToDelete.push(src.slice(1));
-              return `<script${beforeSrc}${afterSrc}>${source.code}</script>`;
-            }
+          const filePath = resolve(root, outDir, name);
+          await writeFile(
+            filePath,
+            chunk.source.replaceAll(
+              /<script([\s\S]*?) src=["']([\s\S]*?)["']([\s\S]*?)>[\s\S]*?<\/script>/gi,
+              (_, beforeSrc: string, src: string, afterSrc: string) => {
+                const source = bundle[src.slice(1)];
+                if (!source || source.type !== "chunk")
+                  throw new Error("Cannot find script chunk");
+                bundlesToDelete.push(src.slice(1));
+                return `<script${beforeSrc}${afterSrc}>${source.code}</script>`;
+              }
+            )
           );
         }
       }
       for (const name of bundlesToDelete) {
-        // eslint-disable-next-line no-param-reassign
-        delete bundle[name];
+        const filePath = resolve(root, outDir, name);
+        await unlink(filePath);
       }
     },
   };
