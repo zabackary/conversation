92ea82ad2466bf247f8d63d6d9f075161f7e25b9
Zachary C <not.an.email@example.com>
2023-05-21 17:20:05 +0900

    feat: Properly throw in usePromise

--- src/client/hooks/usePromise.ts
+++ src/client/hooks/usePromise.ts
@@ -1,19 +1,28 @@
 import { useEffect, useMemo, useState } from "react";
 
-export default function usePromise<T>(promiseFactory: () => Promise<T>) {
-  const [value, setValue] = useState<T | null>(null);
+export default function usePromise<T>(
+  promiseFactory: () => Promise<T>
+): T | null {
+  const [value, setValue] = useState<{ value: T } | { error: unknown } | null>(
+    null
+  );
   const promise = useMemo(() => {
     return promiseFactory();
   }, [promiseFactory]);
   useEffect(() => {
     promise
       .then((newValue) => {
-        setValue(newValue);
+        setValue({ value: newValue });
       })
       .catch((e) => {
-        setValue(null);
-        throw e;
+        setValue({ error: e });
       });
   }, [promise]);
-  return value;
+  if (value && "error" in value) {
+    throw value.error;
+  } else if (value && "value" in value) {
+    return value.value;
+  } else {
+    return null;
+  }
 }
