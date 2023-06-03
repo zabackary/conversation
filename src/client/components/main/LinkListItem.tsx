615681697f50018126d876c176327de3264f82b2
Zachary C <not.an.email@example.com>
2023-06-03 14:13:21 +0900

    feat: Allow LinkListItem to accept refs

--- src/client/components/main/LinkListItem.tsx
+++ src/client/components/main/LinkListItem.tsx
@@ -7,10 +7,10 @@ import {
   ListItemText,
   Skeleton,
 } from "@mui/material";
-import { ReactNode } from "react";
+import { ReactNode, forwardRef } from "react";
 import { Link, useMatches } from "react-router-dom";
 
-interface Props {
+export interface LinkListItemProps {
   primaryText?: string;
   secondaryText?: string;
   to?: string;
@@ -20,74 +20,81 @@ interface Props {
   badge?: number;
   exclude?: number;
 }
-export default function LinkListItem({
-  primaryText,
-  secondaryText,
-  to,
-  icon: dynamicIcon,
-  loading = false,
-  badge,
-  avatar,
-  exclude = 2,
-}: Props) {
-  const matches = useMatches().slice(exclude);
-  const isMatch = matches.find((match) => match.pathname === to);
-  const icon =
-    typeof dynamicIcon === "function" ? dynamicIcon(!!isMatch) : dynamicIcon;
+const LinkListItem = forwardRef<HTMLLIElement, LinkListItemProps>(
+  (
+    {
+      primaryText,
+      secondaryText,
+      to,
+      icon: dynamicIcon,
+      loading = false,
+      badge,
+      avatar,
+      exclude = 2,
+    },
+    ref
+  ) => {
+    const matches = useMatches().slice(exclude);
+    const isMatch = matches.find((match) => match.pathname === to);
+    const icon =
+      typeof dynamicIcon === "function" ? dynamicIcon(!!isMatch) : dynamicIcon;
 
-  return (
-    <ListItem disablePadding sx={{ my: 0.5 }}>
-      {loading || !to ? (
-        <ListItemButton disabled sx={{ opacity: "1 !important" }}>
-          <ListItemIcon>
-            <Skeleton variant="circular" width={24} height={24} />
-          </ListItemIcon>
+    return (
+      <ListItem disablePadding sx={{ my: 0.5 }} ref={ref}>
+        {loading || !to ? (
+          <ListItemButton disabled sx={{ opacity: "1 !important" }}>
+            <ListItemIcon>
+              <Skeleton variant="circular" width={24} height={24} />
+            </ListItemIcon>
 
-          <ListItemText
-            primary={<Skeleton />}
-            secondary={<Skeleton />}
-            primaryTypographyProps={{
-              sx: {
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              },
-              noWrap: true,
-            }}
-            secondaryTypographyProps={{
-              variant: "caption",
-              noWrap: true,
-            }}
-          />
-        </ListItemButton>
-      ) : (
-        <ListItemButton selected={!!isMatch} component={Link} to={to}>
-          {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
-          {avatar ? <ListItemAvatar>{avatar}</ListItemAvatar> : null}
-          <ListItemText
-            primary={primaryText}
-            secondary={secondaryText}
-            primaryTypographyProps={{
-              noWrap: true,
-            }}
-            secondaryTypographyProps={{
-              variant: "caption",
-              noWrap: true,
-            }}
-            sx={{
-              marginRight: badge ? 2 : undefined,
-            }}
-          />
-          {badge ? (
-            <Badge
-              badgeContent={badge}
-              color="primary"
-              max={99}
-              sx={{ marginRight: 1 }}
+            <ListItemText
+              primary={<Skeleton />}
+              secondary={<Skeleton />}
+              primaryTypographyProps={{
+                sx: {
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                },
+                noWrap: true,
+              }}
+              secondaryTypographyProps={{
+                variant: "caption",
+                noWrap: true,
+              }}
             />
-          ) : null}
-        </ListItemButton>
-      )}
-    </ListItem>
-  );
-}
+          </ListItemButton>
+        ) : (
+          <ListItemButton selected={!!isMatch} component={Link} to={to}>
+            {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+            {avatar ? <ListItemAvatar>{avatar}</ListItemAvatar> : null}
+            <ListItemText
+              primary={primaryText}
+              secondary={secondaryText}
+              primaryTypographyProps={{
+                noWrap: true,
+              }}
+              secondaryTypographyProps={{
+                variant: "caption",
+                noWrap: true,
+              }}
+              sx={{
+                marginRight: badge ? 2 : undefined,
+              }}
+            />
+            {badge ? (
+              <Badge
+                badgeContent={badge}
+                color="primary"
+                max={99}
+                sx={{ marginRight: 1 }}
+              />
+            ) : null}
+          </ListItemButton>
+        )}
+      </ListItem>
+    );
+  }
+);
+
+export default LinkListItem;
