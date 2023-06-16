820b812177e5cbebde228e67ae3760101443d748
Zachary C <not.an.email@example.com>
2023-06-16 14:33:24 +0900

    feat: Add a more descriptive 404 page

--- src/client/components/error/error.tsx
+++ src/client/components/error/error.tsx
@@ -1,6 +1,27 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Link as MuiLink,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import normalizeException from "normalize-exception";
-import { useLocation, useRouteError } from "react-router-dom";
+import {
+  Link,
+  isRouteErrorResponse,
+  useLocation,
+  useRouteError,
+} from "react-router-dom";
 import ErrorPage from "./display";
+
+// TODO: translate instead of hardcoding
+const ERROR_MESSAGES: Record<number, string> = {
+  404: "We couldn't find what you were looking for.",
+  500: "Something went wrong on our end.",
+  400: "Something went wrong on your end. Check the sent data.",
+};
 
 export default function RouteErrorPage() {
   const routeError = useRouteError();
@@ -24,6 +45,48 @@ Traceback:
 ${error.stack || JSON.stringify(error)}`}
         traceback={error.stack || JSON.stringify(error)}
       />
+    );
+  }
+  if (isRouteErrorResponse(routeError)) {
+    // Not found, etc. error (404)
+    console.log(routeError);
+    return (
+      <>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography component="h1" variant="h6">
+              Conversation / {routeError.status}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { md: "flex" } }} />
+            <Button color="inherit" component={Link} to="/">
+              Back to main app
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <Container maxWidth="md" sx={{ p: 2 }}>
+          <Typography component="h2" variant="h2">
+            Oops.
+          </Typography>
+          <Typography component="h3" variant="h4" my={2}>
+            {ERROR_MESSAGES[routeError.status] || routeError.statusText}{" "}
+            <small>({routeError.status})</small>
+          </Typography>
+          {routeError.status === 404 ? (
+            <Typography my={2}>
+              Try checking the{" "}
+              <MuiLink component={Link} to="/app">
+                main app
+              </MuiLink>{" "}
+              or{" "}
+              <MuiLink component={Link} to="/">
+                the homepage
+              </MuiLink>
+              .
+            </Typography>
+          ) : null}
+        </Container>
+      </>
     );
   }
   // Route error
