541f137b87468312ca232a6ab52714d0488a8d71
zabackary <137591653+zabackary@users.noreply.github.com>
2023-07-06 08:31:10 +0000

    feat: Add DM join screen!

--- src/client/routes/root/dms/NoDmSelectedRoute.tsx
+++ src/client/routes/root/dms/NoDmSelectedRoute.tsx
@@ -1,33 +1,171 @@
-import { Box, List, Typography, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  List,
+  Stack,
+  Typography,
+  darken,
+  lighten,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { useCallback, useState } from "react";
+import MaterialSymbolIcon from "../../../components/MaterialSymbolIcon";
 import { ConversationAppBar } from "../../../components/layout";
 import { ChannelList } from "../../../components/main";
+import useBackend from "../../../hooks/useBackend";
 import useDMs from "../../../hooks/useDMs";
+import usePromise from "../../../hooks/usePromise";
+import ChannelCard from "../channels/channel/join/ChannelCard";
 
 export default function NoDmSelectedRoute() {
   const theme = useTheme();
   const isMobile = !useMediaQuery(theme.breakpoints.up("sm"));
   const dms = useDMs();
-
-  if (isMobile) {
-    return (
-      <>
-        <ConversationAppBar title="DMs" />
-        <List>
-          <ChannelList channels={dms} />
-        </List>
-      </>
-    );
-  }
+  const backend = useBackend();
+  const [refreshCount, setRefreshCount] = useState(0);
+  const getInvitedChannels = useCallback(
+    () => backend.getInvitedChannels(0, 20),
+    // I want to run the promise every time refreshCount changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [backend, refreshCount]
+  );
+  const invitedChannels = usePromise(getInvitedChannels)?.filter(
+    (value) => value.dm
+  ); // TODO: Add paging
+  const handleRefresh = () => {
+    setRefreshCount((count) => count + 1);
+  };
+  const handleAccept = async (id: number) => {
+    await backend.acceptInvite(id);
+    handleRefresh();
+  };
+  const handleReject = async (id: number) => {
+    await backend.deleteInvite(id);
+    handleRefresh();
+  };
+  const surfaceColor =
+    theme.palette.mode === "dark"
+      ? darken(theme.palette.primary.main, 0.8)
+      : lighten(theme.palette.primary.main, 0.9);
+  const isEmptyState = invitedChannels?.length === 0;
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexGrow="1"
-    >
-      <Typography variant="h5" component="h2">
-        Select a user to get started.
-      </Typography>
-    </Box>
+    <Stack justifyContent="center">
+      {isMobile ? (
+        <>
+          <ConversationAppBar title="DMs" />
+          <List>
+            <ChannelList channels={dms} />
+          </List>
+        </>
+      ) : (
+        <Typography variant="h5" component="h2" mb={4} textAlign="center">
+          Select a user to get started.
+        </Typography>
+      )}
+      <Card sx={{ mx: 2 }} variant={isMobile ? "outlined" : undefined}>
+        <CardHeader
+          title="New requests"
+          action={
+            isMobile ? (
+              <IconButton onClick={handleRefresh}>
+                <MaterialSymbolIcon icon="refresh" />
+              </IconButton>
+            ) : (
+              <Button
+                variant="tonal"
+                startIcon={<MaterialSymbolIcon icon="refresh" />}
+                onClick={handleRefresh}
+              >
+                Refresh
+              </Button>
+            )
+          }
+        />
+        <CardContent>
+          <Box
+            sx={{
+              position: "relative",
+              "&::before, &::after": {
+                position: "absolute",
+                width: 16,
+                top: 0,
+                bottom: 0,
+                content: '""',
+                zIndex: 1,
+              },
+              "&::before": {
+                left: 0,
+                background: `linear-gradient(to right, ${
+                  isMobile ? theme.palette.background.default : surfaceColor
+                }, transparent)`,
+              },
+              "&::after": {
+                right: 0,
+                background: `linear-gradient(to left, ${
+                  isMobile ? theme.palette.background.default : surfaceColor
+                }, transparent)`,
+              },
+            }}
+          >
+            {isEmptyState ? (
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  bottom: 0,
+                  content: '""',
+                  zIndex: 1,
+                  left: 0,
+                  minWidth: "50%",
+                  background: `linear-gradient(to right, ${
+                    isMobile ? theme.palette.background.default : surfaceColor
+                  }, transparent)`,
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <Typography variant="h6" mb={1}>
+                  No new requests
+                </Typography>
+                <Typography>
+                  It looks like you don&apos;t have any new DM requests at the
+                  moment. Check back later!
+                </Typography>
+              </Box>
+            ) : null}
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{
+                overflowX: isEmptyState || !invitedChannels ? "hidden" : "auto",
+                opacity: isEmptyState ? 0.4 : 1,
+                px: 2,
+                pb: 2,
+              }}
+            >
+              {isEmptyState || !invitedChannels
+                ? new Array(14).fill(null).map((_, i) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <ChannelCard key={i} disablePulse={isEmptyState} />
+                  ))
+                : invitedChannels.map((invite) => (
+                    <ChannelCard
+                      key={invite.id}
+                      invite={invite}
+                      handleAccept={handleAccept}
+                      handleReject={handleReject}
+                    />
+                  ))}
+            </Stack>
+          </Box>
+        </CardContent>
+      </Card>
+    </Stack>
   );
 }
